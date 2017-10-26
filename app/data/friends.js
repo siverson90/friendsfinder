c013d801c1bb3ac7e6b44c5e8b50d9dbe938f674
Joshua Siverson <joshuasiverson@Joshuas-MacBook-Air.local>
2017-10-25 20:45:28 -0700

    adding logic for modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friendsData = [
 {
-  "name":"Josh",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "name":"Leonardo Dicaprio",
+  "photo":"https://media1.popsugar-assets.com/files/thumbor/W80K7GHPfO1etD_UuMWdtazY28o/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2017/08/31/609/n/1922398/51dff4a959a811242e61f0.92516557_edit_img_cover_file_17053399_1504185000/i/Celebrities-Who-Have-Donated-Hurricane-Harvey.jpg",
   "scores":[
       5,
       1,
@@ -16,34 +16,67 @@ var friendsData = [
     ]
 },
 {
-  "name":"James",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "name":"Howard Stern",
+  "photo":"http://media.beliefnet.com/~/media/photos-with-attribution/entertainment/celebrities/richest%20celebrities/howardsterncreditsbukleyshutterstockcom.jpg?h=318",
   "scores":[
-      3,
-      2,
-      4,
-      5,
-      2,
+      1,
+      1,
+      1,
+      1,
+      1,
       1,
       2,
       1,
       4,
       1
     ]
-},{
-  "name":"Jangmi",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+},
+{
+  "name":"Miley Cyrus",
+  "photo":"http://www.hollywoodreporter.com/sites/default/files/custom/Meena/miley_cyrus_embed.jpg",
   "scores":[
       5,
-      1,
-      1,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5
+    ]
+},
+{
+  "name":"Jennifer Lawerence",
+  "photo":"http://www.media2.hw-static.com/wp-content/uploads/59131789.jpg",
+  "scores":[
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3
+    ]
+},
+{
+  "name":"David Beckham",
+  "photo":"http://akns-images.eonline.com/eol_images/Entire_Site/201592/rs_1024x759-151002083946-1024-david-beckham-goodwill.ls.10215.jpg",
+  "scores":[
+      4,
+      4,
+      4,
+      4,
       3,
       4,
+      5,
+      1,
       2,
-      4,
-      1,
-      1,
-      1
+      3
     ]
 }
 ];
