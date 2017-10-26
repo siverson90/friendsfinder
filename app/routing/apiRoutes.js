2bf91575532ba1bbb2066bc4f178326f35bcb20f
Joshua Siverson <joshuasiverson@Joshuas-MacBook-Air.local>
2017-10-26 16:50:14 -0700

    now logic will iterate through each question of user and previous friend

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -13,7 +13,19 @@ module.exports = function(app) {
     
 
     var resultScore = req.body.scores;
-    var scoreArray = Array.from(resultScore);
+
+    console.log("starting value " + resultScore);
+
+    // score of the current user who entered they details
+    var scoreArray = resultScore.split("");
+
+    // Array that holds all integers that were converted from survey
+    var userScoreInt = [];
+
+     for (var i = 0; i < scoreArray.length; i++) {
+       var newNumber = parseInt(scoreArray[i]);
+       userScoreInt.push(newNumber);
+    }
 
     var newUser = {
       "name": req.body.name,
@@ -29,48 +41,49 @@ module.exports = function(app) {
     var closetMatch = false;
     var closetMatchName = "";
     var closetMatchPicture = "";
-    var delta = "";
-
+    // starting delta, must be less than 50
+    var delta =0;
     // Do a sum of the new survey result and hold in a variable
 
     // Loop through the previous database summing up their total
 
     // Variable that holds the closet match 
 
-    for (var i = 0; i < scoreArray.length; i++) {
-      currentSurveySum += parseInt(scoreArray[i]);
-    }
-    console.log("Current User sum= "+ currentSurveySum);
+   
+    // console.log("Current User sum= "+ currentSurveySum);
 
     // outter loop going through each database score
     // Use length-1 so that it doesn't compare its own score
     for( var i = 0; i < friendsData.length-1; i++) {
-      var previousUser = 0;
+      // var previousUser = 0;
+      console.log(closetMatch);
       // inner for loop adding each score in array
-      for(var j = 0; j < friendsData[i].scores.length; j++) {
-        previousUser += friendsData[i].scores[j];
-      }
+      forLoopDelta = 0;
 
-      delta = Math.abs((parseInt(previousUser)) - (parseInt(currentSurveySum)));
-      console.log(friendsData[i].name + " sum was " + previousUser);
-      console.log("The delta is " + delta);
+      for(var j = 0; j < friendsData[i].scores.length; j++) {
+
+        var diff = userScoreInt[j] - friendsData[i].scores[j];
+        var absValue = Math.abs(diff);
+        forLoopDelta += absValue;
+      }
+      console.log(friendsData[i].name + "had a total delta of " + forLoopDelta);
 
       // Look at the sums of array and push to variable is closest
       if(closetMatch === false) {
-        closetMatch = delta;
+        closetMatch = true;
+        delta = forLoopDelta;
         closetMatchName = friendsData[i].name;
         closetMatchPicture = friendsData[i].photo;
         console.log(friendsData[i].name + " is the closet match with a delta of " + delta);
       }
-      else if(delta < closetMatch) {
-        closetMatch = delta;
+      else if(forLoopDelta < delta) {
+        delta = forLoopDelta;
         closetMatchName = friendsData[i].name;
         closetMatchPicture = friendsData[i].photo;
         console.log( friendsData[i].name + " is the closet match with a delta of " + delta);
       }
     }
-    // console.log("Here is your match");
-    // console.log("You match closest with " + clo)
+
       res.json({
         "name": closetMatchName,
         "photo": closetMatchPicture,
